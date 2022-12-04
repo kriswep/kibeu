06bdea783e288f5ac4222176bec831a0d30c1b05
kriswep <kriswep@wetainment.com>
2022-12-04 20:19:14 +0100

    configure prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-    <h1 className="text-7xl font-bold underline">
-      Hello, Next.js!
-    </h1>
-  )
+  return <h1 className="text-7xl font-bold underline">Hello, Next.js!</h1>;
 }
