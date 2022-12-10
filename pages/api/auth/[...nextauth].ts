91606a4054051f088c740efde2a1f813428c572b
kriswep <kriswep@wetainment.com>
2022-12-10 23:40:00 +0100

    sign in with hasura user

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth, { NextAuthOptions, User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { JWT } from 'next-auth/jwt';
 import * as jsonwebtoken from 'jsonwebtoken';
+import * as argon2 from 'argon2';
+import { request, gql } from 'graphql-request';
 
 export const authOptions: NextAuthOptions = {
   // Use JWT strategy so we can forward them to Hasura
@@ -26,46 +28,51 @@ export const authOptions: NextAuthOptions = {
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        email: { label: 'E-Mail', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
-        // // You need to provide your own logic here that takes the credentials
-        // // submitted and returns either a object representing a user or value
-        // // that is false/null if the credentials are invalid.
-        // // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // // You can also use the `req` object to obtain additional parameters
-        // // (i.e., the request IP address)
-        // const res = await fetch('/your/endpoint', {
-        //   method: 'POST',
-        //   body: JSON.stringify(credentials),
-        //   headers: { 'Content-Type': 'application/json' },
-        // });
-        // const user = await res.json();
+        console.log(credentials);
+        const query = gql`
+          query GetUser($email: String!) {
+            users(where: { email: { _eq: $email } }, limit: 1) {
+              id
+              password
+              email
+            }
+          }
+        `;
+        try {
+          const { users } = await request(
+            process.env.HASURA_GRAPHQL_ENDPOINT!,
+            query,
+            { email: credentials?.email },
+            { 'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET! },
+          );
+          const user = users[0] as User;
 
-        // // If no error and we have user data, return it
-        // if (res.ok && user) {
-        //   return user;
-        // }
-        // // Return null if user data could not be retrieved
-        // return null;
-        // Add logic here to look up the user from the credentials supplied
-        const user = {
-          id: 'e1f457a4-f12e-46c3-9e94-ec1cd362e9dd',
-          name: 'J Smith',
-          email: 'jsmith@example.com',
-          extra: 'info',
-        };
-        // throw new Error('Test');
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
+          if (
+            user.id.length > 0 &&
+            user.email === credentials?.email &&
+            (await argon2.verify(
+              users[0].password,
+              credentials?.password || '',
+            ))
+          ) {
+            return {
+              id: user.id,
+              email: user.email,
+            };
+          } else {
+            // console.error('Invalid Password');
+            return null;
+          }
+        } catch (error) {
+          console.error(JSON.stringify(error, undefined, 2));
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
+        return null;
+        //   // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
       },
     }),
     // ...add more providers here
