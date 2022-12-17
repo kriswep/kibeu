9268688199a31850b2c1d9a62a576bd71c5c5cf9
kriswep <kriswep@wetainment.com>
2022-12-17 21:52:30 +0100

    add children data model

---
+++ hasura/migrations/pg/1671309416221_add childrens/down.sql
@@ -0,0 +1,4 @@
+
+alter table "public"."children" rename column "account_id" to "account";
+
+DROP TABLE "public"."children";
---
+++ hasura/migrations/pg/1671309416221_add childrens/up.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE "public"."children" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "account" uuid NOT NULL, "name" text NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , FOREIGN KEY ("account") REFERENCES "public"."users"("id") ON UPDATE cascade ON DELETE restrict, UNIQUE ("id"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_children_updated_at"
+BEFORE UPDATE ON "public"."children"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_children_updated_at" ON "public"."children" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+alter table "public"."children" rename column "account" to "account_id";
